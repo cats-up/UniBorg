6ad535c734a796e09901bc6646550295539cb339
cats-up <54433617+cats-up@users.noreply.github.com>
2019-09-27 18:01:36 +0600

    Update remove.bg.py

--- stdplugins/remove.bg.py
+++ stdplugins/remove.bg.py
@@ -72,7 +72,7 @@ async def _(event):
             )
         end = datetime.now()
         ms = (end - start).seconds
-        await event.edit("Background Removed in {} seconds)
+        await event.edit("Background Removed in {} seconds".format(ms))
     else:
         await event.edit("ReMove.BG API returned Errors. Please report to @UniBorg\n`{}".format(output_file_name.content.decode("UTF-8")))
 
